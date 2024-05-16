058992a5ada3d5d66884f211ac69d5eb8344ba50
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-05-16 10:41:22 +0330

    Task2 added

---
+++ Lab03/Task/Task2.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# create a 3 x 2 matrix
+A = np.array([[1, 2],
+              [3, 6],
+              [-2,-4]])
+
+fig = plt.figure()
+
+# A 1 by 2 subplot grid, subplot 1 (3D)
+ax1 = fig.add_subplot(1,2,2, projection='3d')
+ax1.set_title('column space')
+
+
+# ----------------Create Column Space--------------------
+
+# creat a random column vector
+u = np.random.randn(2,200)
+
+# create a point in the column space of A
+v = A @ u
+
+ax1.scatter(v[0,:], v[1,:], v[2,:], color='b')
+        
+
+# A 1 by 2 subplot grid, subplot 2 (2D)
+ax2 = fig.add_subplot(1,2,1)
+ax2.set_title('row space')
+
+
+# ----------------Create Row Space--------------------
+
+# creat a random row vector
+u = np.random.randn(200,3)
+
+# create a point in the row space of A
+v = u @ A
+
+ax2.plot(v[:,0], v[:,1], 'ro')
+
+plt.show()
