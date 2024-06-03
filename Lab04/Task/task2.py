240117a3ab1b47a1b7cc487853f4237ae89bb86c
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-06-03 13:07:25 +0330

    task2 added

--- Lab04/Task/task2.py
+++ Lab04/Task/task2.py
@@ -1,11 +1,32 @@
 import numpy as np
+import timeit
 
-m,n,p = 100,50, 2000
+m,n,p = 2,1, 20
 
 A = np.random.rand(m,n,p) 
 s = np.random.rand(p)
 
-for i in range(p):
-    A[:,:,i] *= s[i]
+print('A=\n', A) 
+print('Shape of A=\n', A.shape)
+print('Shape of s=\n', s.shape)
 
+# B = A*s
+
+def f():
+    for i in range(p):
+        A[:,:,i] *= s[i] # it is deep copy
+    print(A)
+
+f()
+# print(B==A)
+
+def g():
+    A*s
+print(s*A)
+
+# t1 = timeit.timeit(f , number=100)/100
+# t2 = timeit.timeit(g, number=100)/100
+
+# print("t1 = ",t1)
+# print("t2 = ",t2)
 
