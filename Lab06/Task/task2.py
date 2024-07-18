f450eab5902acf44505c0baaf061e62c6f283cbf
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-07-18 21:51:21 +0330

    Task2 added

--- Lab06/Task/task2.py
+++ Lab06/Task/task2.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from face_data import Face1, Face2, Face3, TargetFace2, edges
 
+
 def plot_face(plt,X,edges,color='b'):
     "plots a face"
     
@@ -20,9 +21,11 @@ def plot_face(plt,X,edges,color='b'):
     plt.xlim(-100,100)
     plt.ylim(-100,100)
 
+
 TargetFace = TargetFace2
 NoisyTargetFace = TargetFace + 3 * np.random.randn(*TargetFace.shape)
-    
+# print (*TargetFace.shape)
+#     
 face1 = Face1.ravel()
 face2 = Face2.ravel()
 face3 = Face3.ravel()
@@ -32,9 +35,15 @@ F = np.stack((face1, face2, face3), axis=1)
 
 for i in range(5):
     inds = np.random.choice(range(136), 3, replace=False)
+    print('inds =',inds)
+    # choose the equations
+    f = F[inds,0:3]
+    # print('f =',f)
+    T = t[inds]
+    # print('T =',T)
 
-    a1,b1,c1 = # solve 3 random equations
-    a2,b2,c2 = # least squares solution
+    a1,b1,c1 = np.linalg.solve(f,T) # solve 3 random equations
+    a2,b2,c2 = np.linalg.lstsq(F,t)[0] # least squares solution
     
     Face_rnd = a1 * Face1 + b1 * Face2 + c1 * Face3
     Face_lsq = a2 * Face1 + b2 * Face2 + c2 * Face3
@@ -42,6 +51,8 @@ for i in range(5):
     plot_face(plt, NoisyTargetFace, edges, color='k')
     plot_face(plt, Face_rnd, edges, color='g')
     plot_face(plt, Face_lsq, edges, color='b')
+    print('error_lsq =', np.linalg.norm(Face_lsq - TargetFace))
+    print('error_NoisyT =', np.linalg.norm(NoisyTargetFace - TargetFace))
     
     plt.show()
 
